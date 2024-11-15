31f0383c2ed05b87323752d5ee98ff75577646cc
tejes <tejes@Yugesh>
2024-11-15 09:41:12 +0530

    To Learn GitHub

---
+++ AutomationSelenium/src/GitHub/ToLearnGitHub.java
@@ -0,0 +1,11 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnGitHub {
+	@Test
+	public void git() {
+		Reporter.log("Added class in github package");
+	}
+}
