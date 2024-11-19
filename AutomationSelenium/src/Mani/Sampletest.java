62a19d4fdf262d91722630bbcb37e90b4eec89a8
tejes <tejes@Yugesh>
2024-11-19 12:58:43 +0530

    sample test

---
+++ AutomationSelenium/src/Mani/Sampletest.java
@@ -0,0 +1,11 @@
+package Mani;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sampletest {
+	@Test
+	public void sample() {
+		Reporter.log("Sample page displayed",true);
+	}
+}
